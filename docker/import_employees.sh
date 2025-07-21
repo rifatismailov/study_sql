17785e49accb8676dba7b8e5b21da06cd8e26648
Rifat Ismailov <refaazer@gmail.com>
2025-07-21 18:50:19 +0300

    Study

--- docker/import_employees.sh
+++ docker/import_employees.sh
@@ -4,8 +4,7 @@ set -e
 
 MYSQL_USER="test_user"
 MYSQL_PASSWORD="Kh87Igs87HG"
-EMPLOYEES_DB="employees"
-SAKILA_DB="sakila"
+MYSQL_DATABASE="employees"
 WAIT_SECONDS=25
 BASE_PATH="/var/lib/mysql-files/test_db"
 RAW_URL="https://raw.githubusercontent.com/datacharmer/test_db/master"
@@ -27,31 +26,22 @@ for FILE in $FILE_LIST; do
   curl -s -O "$RAW_URL/$FILE"
 done
 
-SAKILA_PATH="$BASE_PATH/sakila"
-cd "$BASE_PATH"
-
 echo "🛠 Оновлюю шляхи у employees.sql..."
 sed -i 's|source \(load_.*\.dump\)|source /var/lib/mysql-files/test_db/\1|g' employees.sql
 sed -i 's|source show_elapsed.sql|source /var/lib/mysql-files/test_db/show_elapsed.sql|g' employees.sql
 
-echo "🗃 Створюю бази $EMPLOYEES_DB і $SAKILA_DB..."
-mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $EMPLOYEES_DB"
-mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $SAKILA_DB"
+echo "🗃 Створюю базу $MYSQL_DATABASE..."
+mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE"
 
 echo "🔑 Надаю доступ користувачу $MYSQL_USER..."
 mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "
   CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-  GRANT ALL PRIVILEGES ON $EMPLOYEES_DB.* TO '$MYSQL_USER'@'%';
-  GRANT ALL PRIVILEGES ON $SAKILA_DB.* TO '$MYSQL_USER'@'%';
+  GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
   GRANT RELOAD ON *.* TO '$MYSQL_USER'@'%';
   FLUSH PRIVILEGES;
 "
 
-echo "📥 Імпортую базу $EMPLOYEES_DB..."
-mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$EMPLOYEES_DB" < employees.sql
+echo "📥 Імпортую базу..."
+mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < employees.sql
 
-echo "📥 Імпортую базу $SAKILA_DB..."
-mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$SAKILA_DB" < "$SAKILA_PATH/sakila-mv-schema.sql"
-mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$SAKILA_DB" < "$SAKILA_PATH/sakila-mv-data.sql"
-
-echo "✅ Успішно встановлено бази employees та sakila!"
+echo "✅ Готово!"
