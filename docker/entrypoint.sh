dd216cddef7c96d0f9987dde2397e58dc8538d64
Rifat Ismailov <refaazer@gmail.com>
2025-07-25 00:13:39 +0300

    Study

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "⬇️ Downloading the latest import_employees.sh from GitHub..."
+echo "⬇️ Завантажую свіжий import_employees.sh з GitHub..."
 curl -s -o /docker-entrypoint-initdb.d/import_employees.sh \
-    https://raw.githubusercontent.com/rifatismailov/study_sql/main/docker/import_employees.sh \
-    && chmod +x /docker-entrypoint-initdb.d/import_employees.sh
+     https://raw.githubusercontent.com/rifatismailov/study_sql/main/docker/import_employees.sh \
+  && chmod +x /docker-entrypoint-initdb.d/import_employees.sh
 
-# Start MySQL
-exec docker-entrypoint.sh "$@"
+# 🔁 ВАЖЛИВО: викликаємо офіційний оригінальний entrypoint
+exec /usr/local/bin/docker-entrypoint.sh "$@"
